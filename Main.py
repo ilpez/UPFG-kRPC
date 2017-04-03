dd395f001c701122c3acd71c7b34bb5be6297a2a
ilpez <ilham.pezek@gmail.com>
2017-04-04 07:55:10 +0800

    Replace staging with function

--- Main.py
+++ Main.py
@@ -35,7 +35,7 @@ elif sys.argv[6] == 'EXP':
     meco_speed = 2700
     turn_speed = 50
 else:
-    meco_speed = 3000
+    meco_speed = vehicle[0].l1
     turn_speed = 50
 print(meco_speed)
 [azimuth, launch_time, target] = upfg.launchTargeting(target_periapsis,
@@ -52,15 +52,8 @@ while (space_center.ut - game_launch_time) < 0:
     time.sleep(1)
 
 vessel.control.throttle = 1
-for engine in vessel.parts.engines:
-    if not engine.active:
-        print('There is no active engine, checking Propellant condition')
-        for engine in engine.part.modules:
-            if engine.has_field('Propellant'):
-                if engine.get_field('Propellant') == 'Very Stable':
-                    print('Engine is ready')
-                    vessel.control.activate_next_stage()
-while vessel.thrust < vessel.available_thrust:
+vessel.control.activate_next_stage()
+while vessel.thrust < Global.state_mass() * g0:
     time.sleep(0.01)
 
 vessel.auto_pilot.engage()
@@ -98,22 +91,7 @@ while True:
 
 print('Main Engine Cutoff')
 
-vessel.control.activate_next_stage()
-vehicle.remove(vehicle[0])
-vessel.control.rcs = True
-vessel.control.forward = 1
-time.sleep(2)
-
-for engine in vessel.parts.engines:
-    if not engine.active:
-        print('There is no active engine, checking Propellant condition')
-        for engine in engine.part.modules:
-            if engine.has_field('Propellant'):
-                if engine.get_field('Propellant') == 'Very Stable':
-                    print('Engine is ready')
-                    vessel.control.forward = 0
-                    vessel.control.throttle = 1
-                    vessel.control.activate_next_stage()
+upfg.stageController(vehicle, ullage=False)
 
 while vessel.thrust < vessel.available_thrust:
     time.sleep(0.01)
@@ -166,7 +144,7 @@ while True:
     if t > 1:
         vessel.auto_pilot.target_heading = upfg_guided.yaw
         vessel.auto_pilot.target_pitch = upfg_guided.pitch
-    if upfg_guided.tgo < 0.1:
+    if t < 0.1:
         vessel.control.throttle = 0
         break
     if Global.orbital_speed() > target.velocity:
@@ -178,5 +156,4 @@ while True:
                 if module.has_event('Jettison'):
                     module.trigger_event('Jettison')
                     fairing_jettison = True
-
 print('Mission Success')
